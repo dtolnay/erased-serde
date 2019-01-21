821ef0677a06f79101052e5fab977cca867184ec
David Tolnay <dtolnay@gmail.com>
2019-01-20 22:17:56 -0800

    Release 0.3.7

--- src/lib.rs
+++ src/lib.rs
@@ -88,7 +88,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/erased-serde/0.3.6")]
+#![doc(html_root_url = "https://docs.rs/erased-serde/0.3.7")]
 
 #![cfg_attr(feature = "unstable-debug", feature(core_intrinsics))]
 
