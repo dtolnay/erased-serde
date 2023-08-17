2237513fe0065e96f5164cd983ee5ad8fe8618a7
David Tolnay <dtolnay@gmail.com>
2023-08-16 18:17:38 -0700

    Partially work around ridiculous rust-analyzer behavior

--- src/private.rs
+++ src/private.rs
@@ -1,9 +1,14 @@
 //! Not public API. Used as `$crate::__private` by macros.
 
+#[doc(hidden)]
 pub use core::marker::{Send, Sized, Sync};
-pub use core::result::Result;
+#[doc(hidden)]
 pub use serde;
 
+#[doc(hidden)]
+pub type Result<T, E> = core::result::Result<T, E>;
+
+#[doc(hidden)]
 pub fn require_erased_serialize_impl<T>()
 where
     T: ?Sized + crate::Serialize,
