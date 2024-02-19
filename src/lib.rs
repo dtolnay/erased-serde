427a8a74cc8b0294163f05b666070ba70cad869c
David Tolnay <dtolnay@gmail.com>
2024-02-18 21:25:35 -0800

    Release 0.4.3

--- src/lib.rs
+++ src/lib.rs
@@ -95,7 +95,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/erased-serde/0.4.2")]
+#![doc(html_root_url = "https://docs.rs/erased-serde/0.4.3")]
 #![no_std]
 #![deny(unsafe_op_in_unsafe_fn)]
 #![allow(
