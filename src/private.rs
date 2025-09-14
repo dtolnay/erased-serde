66d7886a2d2b4d8dade1e66bc7159f61e2e94218
David Tolnay <dtolnay@gmail.com>
2025-09-13 18:14:37 -0700

    Work around erased_serde::__private appearing in suggestions

--- src/private.rs
+++ src/private.rs
@@ -1,9 +1,13 @@
 //! Not public API. Used as `$crate::__private` by macros.
 
 #[doc(hidden)]
-pub use core::marker::{Send, Sized, Sync};
+pub mod serde {
+    #[doc(hidden)]
+    pub use serde::*;
+}
+
 #[doc(hidden)]
-pub use serde_core as serde;
+pub use core::marker::{Send, Sized, Sync};
 
 #[doc(hidden)]
 pub type Result<T, E> = core::result::Result<T, E>;
